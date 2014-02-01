6812b53a72c9d4553596e37726202c8a376e51f4
Rafal Gawel <rafal.gawel@gmail.com>
2014-02-01 22:07:44 +0100

    Adding sfml

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,27 @@
 #include "UnitTest++/UnitTest++.h"
 
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
 int main()
 {
-	return UnitTest::RunAllTests();
+	sf::RenderWindow window(sf::VideoMode(200, 200), "hydra");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
 }
